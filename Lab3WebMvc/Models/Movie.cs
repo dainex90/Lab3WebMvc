6319dd4d3a47aa33c3bcd1799b9d25fca08af770
Daniel <Daniel@LAPTOP-FMH5E13S>
2018-03-26 14:11:28 +0200

    Created Movie Entity

---
+++ Lab3WebMvc/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3WebMvc.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set;}
+        public float Starting { get; set;}
+        public virtual IList<Ticket> Tickets { get; set;}
+    }
+}
---
+++ Lab3WebMvc/Models/Ticket.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3WebMvc.Models
+{
+    public class Ticket
+    {
+    }
+}
